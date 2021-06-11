904900c52a4651156e0af94fc5869d9538131073
jhondev123 <jhonattan.curtarelli@outlook.com>
2021-06-10 22:38:40 -0300

    Creating PopUps

---
+++ JS/loadModel.js
@@ -0,0 +1,5 @@
+$(function() {
+    $('#header').load("Modelos/header.html");
+    $('#footer').load("Modelos/footer.html");
+    $('#popup').load("Pop-Up/personalizada.html");
+});
---
+++ JS/loadPop-Up.js
@@ -0,0 +1,4 @@
+function initPopUP(modalID) {
+    const modal = document.getElementById(modalID);
+    modal.classList.add('mostrar');
+}
